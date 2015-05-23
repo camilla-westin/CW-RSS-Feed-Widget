d7f0bc410323eef20a6a2ee8d975f2904a04819c
Camilla Westin <camilla@webstin.se>
2015-05-23 16:48:38 +0200

    First commit

---
+++ cw-rss-feed.php
@@ -0,0 +1,27 @@
+<?php 
+
+ //Import RSS feed 
+        if(function_exists('fetch_feed')) {
+          $feed = fetch_feed($rssfeedurl); // specify the source feed
+          $limit = $feed->get_item_quantity($rsspostcount); // specify number of items
+          $items = $feed->get_items(0, $limit); // create an array of rss items
+
+        }
+
+ //Loop and display RSS feed
+        if ($limit == 0) echo '<div>The feed is either empty or unavailable.</div>';
+        else foreach ($items as $item) : ?>
+
+        <li class="rss-feed-listitem">
+            <div class="rss-feed-article-link">
+              <a href="<?php echo $item->get_permalink(); ?>" title="<?php echo $item->get_date('j F Y @ g:i a'); ?>">
+                <?php echo $item->get_title(); ?>
+              </a>
+            </div>
+        
+            <div class="rss-feed-article-desc">
+              <?php echo substr($item->get_description(), 0, 400); ?> 
+            </div>
+        </li>
+
+ <?php endforeach; ?>
---
+++ cw-rss-widget.php
@@ -0,0 +1,109 @@
+<?php
+/*
+Plugin Name: CW RSS Feed Widget
+Description: Displays RSS Feed
+Version: 1.0
+Author: Camilla Westin
+Author URI: http://www.camillawestin.se
+*/
+
+function cw_rss_widget_styles() {
+    wp_enqueue_style( 'cw-rss-plugin-style', plugins_url('/cw-rss-feed-style.css', __FILE__), false, '1.0.0', 'all' );
+  }
+
+  add_action('wp_enqueue_scripts', 'cw_rss_widget_styles');
+
+class cw_rss_widget extends WP_Widget {
+
+	function __construct() {
+      parent::__construct(
+      // Base ID of your widget
+      'cw_rss_widget', 
+
+      // Widget name will appear in UI
+      __('CW RSS Feed', 'wp_cw_rss_widget'), 
+
+      // Widget description
+      array( 'description' => __( 'Displays RSS Feed', 'wp_cw_rss_widget' ), ) 
+      );
+  }
+
+	// widget form creation
+function form($instance) {
+
+// Check values
+if( $instance) {
+     $title = esc_attr($instance['title']);
+     $rsspostcount = esc_attr($instance['rsspostcount']);
+     $rssfeedurl = esc_attr($instance['rssfeedurl']);
+
+} else {
+     $title = '';
+     $rsspostcount = '';
+     $rssfeedurl = '';
+  }
+?>
+<p>
+<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'wp_cw_rss_widget'); ?></label>
+<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+</p>
+
+<p>
+<label for="<?php echo $this->get_field_id('rsspostcount'); ?>"><?php _e('Number of posts:', 'wp_cw_rss_widget'); ?></label>
+<input class="widefat" id="<?php echo $this->get_field_id('rsspostcount'); ?>" name="<?php echo $this->get_field_name('rsspostcount'); ?>" type="text" value="<?php echo $rsspostcount; ?>" />
+</p>
+
+<p>
+<label for="<?php echo $this->get_field_id('rssfeedurl'); ?>"><?php _e('RSS Feed URL:', 'wp_cw_rss_widget'); ?></label>
+<input class="widefat" id="<?php echo $this->get_field_id('rssfeedurl'); ?>" name="<?php echo $this->get_field_name('rssfeedurl'); ?>" type="text" value="<?php echo $rssfeedurl; ?>" />
+</p>
+
+
+<?php
+}
+
+	// update widget
+function update($new_instance, $old_instance) {
+      $instance = $old_instance;
+      // Fields
+      $instance['title'] = strip_tags($new_instance['title']);
+      $instance['rsspostcount'] = strip_tags($new_instance['rsspostcount']);
+      $instance['rssfeedurl'] = strip_tags($new_instance['rssfeedurl']);
+
+    return $instance;
+}
+
+	// display widget
+function widget($args, $instance) {
+   extract( $args );
+   // these are the widget options
+   $title = $instance['title'];
+   $rsspostcount = $instance['rsspostcount'];
+   $rssfeedurl = $instance['rssfeedurl'];
+   echo $before_widget;
+
+     // Display the widget
+
+     // Check if title is set
+         if ( $title ) {
+            echo $before_title . $title . $after_title;
+         }
+
+     echo '<div class="cw-rss-widget>';
+     echo '<ul class="rss-feed-list">';
+        
+      //Import and loop rss feed
+      include('cw-rss-feed.php');
+
+      echo '</ul></div>';
+   
+    echo $after_widget;
+  }
+}
+
+// Register and load the widget
+function wpb_load_widget() {
+  register_widget( 'cw_rss_widget' );
+}
+
+add_action( 'widgets_init', 'wpb_load_widget' );
